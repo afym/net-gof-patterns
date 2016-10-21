8a860869158f81b4ba4e00dfd81a80f565599f8f
Angel Ybarhuen <angel.ybarhuen@orbis.com.pe>
2016-10-20 19:42:05 -0500

    feat(behavioral): Adding the Iterator pattern using c# features

---
+++ SoftwarePatterns/Behavioral/Iterator/NamesStack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SoftwarePatterns.Behavioral.Iterator
+{
+    public class NamesStack<T> : IEnumerable<T>
+    {
+        private T[] Names;
+        private int Counter;
+
+        public NamesStack(int Size) 
+        {
+            this.Counter = 0;
+            this.Names = new T[Size];
+        }
+
+        public void Add(T Name) 
+        {
+            this.Names[Counter++] = Name;
+        }
+
+        public T Last() 
+        {
+            return this.Names[Counter - 1];
+        }
+
+        public T First() 
+        {
+            return this.Names[0];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int Index = 0; Index < Counter; Index++)
+            {
+                yield return this.Names[Index];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ SoftwarePatternsTest/Behavioral/Iterator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoftwarePatterns.Behavioral.Iterator;
+
+namespace SoftwarePatternsTest.Behavioral
+{
+    [TestClass]
+    public class Iterator
+    {
+        [TestMethod]
+        public void TestInstance()
+        {
+            NamesStack<string> Names = new NamesStack<string>(100);
+            Assert.IsInstanceOfType(Names, typeof(IEnumerable));
+        }
+
+        [TestMethod]
+        public void TestIteration() 
+        {
+            NamesStack<string> Names = new NamesStack<string>(8);
+
+            for (int Index = 0; Index < 5; Index++)
+            {
+                Names.Add("A string" + Index);
+            }
+
+            int Iterations = 0;
+
+            foreach(string Name in Names)
+            {
+                Iterations++;
+                Assert.IsInstanceOfType(Name, typeof(string));
+            }
+
+            Assert.AreEqual(Iterations, 5);
+            Assert.AreEqual(Names.First(), "A string0");
+            Assert.AreEqual(Names.Last(), "A string4");
+        }
+    }
+}
